64d98f5a9018a0959781ec1291f703a7a3ad0ac8
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-05 15:47:34 +0200

    add-first-section-footer

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,22 @@
   <div id="app">
     <MainHeader />
     <MainContent />
+    <FooterContent />
   </div>
 </template>
 
 <script>
 import MainHeader from './components/MainHeader.vue';
 import MainContent from './components/MainContent.vue';
+import FooterContent from './components/FooterContent.vue';
+
 
 export default {
   name: 'App',
   components: {
     MainHeader,
     MainContent,
+    FooterContent,
 }
 }
 </script>
---
+++ src/components/FooterContent.vue
@@ -0,0 +1,53 @@
+<template>
+    <footer>
+      <section class="footer__first__section">
+        <div class="small__container">
+        <ul>
+          <li>
+            <img src="../assets/buy-comics-digital-comics.png" alt="">
+            <a href="#">digital comics</a>
+          </li>
+        </ul>
+      </div>
+      </section>
+      
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="scss">
+  
+  @import '../style/variables';
+
+  .footer__first__section {
+    background-color: $bg-light-blue;
+
+    .small__container {
+      max-width: 900px;
+      margin: 0 auto;
+      padding: 2rem 0;
+
+      ul {
+        display: flex;
+        align-items: center;
+        li {
+          display:flex;
+          gap: 1rem;
+          align-items: center;
+          color: white;
+          text-transform: uppercase;
+          font-size: $font-size-upper;
+
+          img {
+            width: 40px;
+          } 
+        }
+      }
+    }
+  }
+</style>
