b5839949fd27a7b8d53ea3d6e3b92c1c15e6025a
FabioLongoni <longoni.fabio90@gmail.com>
2022-09-06 13:31:10 +0200

    add-main__content

---
+++ src/components/ProductCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <article class="card">
+    <img :src="thumb" alt="">
+    <h3>{{ series }}</h3>
+  </article>
+</template>
+
+<script>
+export default {
+  props: {
+    thumb: String,
+    series: String,
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+  @import '../style/variables';
+  .card {
+    font-size:$font-size-upper;
+    text-transform: uppercase;
+    cursor: pointer;
+    img {
+      width: 200px;
+      aspect-ratio: 1;
+      object-fit: cover;
+      object-position: top;
+      margin-bottom: 1rem;
+    }
+  }
+</style>
